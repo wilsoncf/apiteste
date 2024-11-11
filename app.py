a7e2e7ec8752994eefddc4d321dc3ca988948c07
Wilson <wilsoncffilho@outlook.com>
2024-11-11 15:03:15 -0300

    fix bug

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def home():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Hello, World!"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
